ddc1a06ba58995f6956049f80b30156eda4071cc
LeeJaeHyeok97 <hazardous10@naver.com>
2024-07-01 06:45:48 +0900

    Feat: JWTFilter 추가

--- src/main/java/com/quizbox/global/config/security/SecurityConfig.java
+++ src/main/java/com/quizbox/global/config/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.quizbox.global.config.security;
 
 import com.quizbox.domain.auth.service.CustomOAuth2UserService;
+import com.quizbox.global.config.security.handler.CustomSuccessHandler;
+import com.quizbox.global.config.security.jwt.JWTFilter;
+import com.quizbox.global.config.security.jwt.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +20,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTUtil jwtUtil;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -32,11 +38,17 @@ public class SecurityConfig {
         http
                 .httpBasic((auth) -> auth.disable());
 
+        //JWTFilter 등록
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
         // oauth2
         http
                 .oauth2Login((oauth2) -> oauth2
                         .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService)));
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)
+                );
 
         // 경로별 인가 작업
         http
